438bc021c58636ece251e8d4276120203da94a6c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 16:01:56 +0300

    coloured picker

--- SegmentedControlPickerNoSegmentSelected/ContentView.swift
+++ SegmentedControlPickerNoSegmentSelected/ContentView.swift
@@ -14,9 +14,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Segmented Control (Picker)").font(.largeTitle)
+            Text("Segmented Control (Picker)")
+                .font(.largeTitle)
+                .layoutPriority(1)
             Text("No Segment Selection").font(.title).foregroundColor(.gray)
             Text("This segment control will have nothing selected because the default state variable does not match any ot the segmented tag values.")
+                .layoutPriority(1)
             Text("How many meals do you eat?")
                 .foregroundColor(.gray)
                 .font(.title)
@@ -30,8 +33,17 @@ struct ContentView: View {
                 .stroke(Color.red, lineWidth: selection == 0 ? 1 : 0))
             .padding()
             Text("The red ouline will go away once a selection is made.")
+            Picker("", selection: $selection) {
+                Text("One").tag(1)
+                Text("Two").tag(2)
+                Text("Three").tag(3)
+                Text("More").tag(4)
+            }.pickerStyle(SegmentedPickerStyle())
+                .background(Color.yellow)
+            .cornerRadius(8)
+                .padding(.horizontal)
             
-        }
+        }.padding()
     }
 }
 
