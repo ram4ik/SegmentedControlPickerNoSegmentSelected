27292defa09739eabc85e974a55fe2948a4723d3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 15:54:50 +0300

    init

--- SegmentedControlPickerNoSegmentSelected/ContentView.swift
+++ SegmentedControlPickerNoSegmentSelected/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+    
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 20) {
+            Text("Segmented Control (Picker)").font(.largeTitle)
+            Text("No Segment Selection").font(.title).foregroundColor(.gray)
+            Text("This segment control will have nothing selected because the default state variable does not match any ot the segmented tag values.")
+            Text("How many meals do you eat?")
+                .foregroundColor(.gray)
+                .font(.title)
+            Picker("", selection: $selection) {
+                Text("One").tag(1)
+                Text("Two").tag(2)
+                Text("Three").tag(3)
+                Text("More").tag(4)
+            }.pickerStyle(SegmentedPickerStyle())
+            .background(RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.red, lineWidth: selection == 0 ? 1 : 0))
+            .padding()
+            Text("The red ouline will go away once a selection is made.")
+            
+        }
     }
 }
 
